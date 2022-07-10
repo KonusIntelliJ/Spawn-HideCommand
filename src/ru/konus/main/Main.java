18c8183486dd643f656f04845c35d467eb23fb9f
KonusIntelliJ <62984001+KonusIntelliJ@users.noreply.github.com>
2022-07-10 19:35:36 +0300

    Update Main.java

--- src/ru/konus/main/Main.java
+++ src/ru/konus/main/Main.java
@@ -8,19 +8,35 @@ import ru.konus.spawn.Spawn;
 import ru.konus.spawn.SpawnListener;
 
 public class Main extends JavaPlugin {
+    
+    private Location spawn;
 
-    public void onEnable(){
-
-        getLogger().info("§aPlugin Enabled");
+    public void onEnable() {
         getCommand("spawn").setExecutor(new Spawn(this));
         getCommand("setspawn").setExecutor(new SetSpawn(this));
         getCommand("hide").setExecutor(new Hide(this));
         Bukkit.getPluginManager().registerEvents(new SpawnListener(this), this);
         Bukkit.getPluginManager().registerEvents(new Hide(this), this);
+        FileConfiguration config = plugin.getConfig();
+        File file = new File(getDataFolder(), "config.yml");
+        if (file.exists()) {
+           String spawnStr = "spawn";
+           double x = config.getDouble(spawnStr + ".x");
+           double y = config.getDouble(spawnStr + ".y");
+           double z = config.getDouble(spawnStr + ".z");
+           float yaw = (float) config.getDouble(spawnStr + ".yaw");
+           float pitch = (float) config.getDouble(spawnStr + ".pitch");
+           spawn = new Location(Bukkit.getWorld("world"), x, y, z, yaw, pitch);
+        } else spawn = null;
+        getLogger().info("§7[§6!§7] §fPlugin §6Spawn §fwas enabled!");
+    }
+    
+    public Location getSpawnLocation() {
+        return spawn;
     }
 
     public void onDisable(){
-        getLogger().info("§cPlugin Disabled");
+        getLogger().info("§7[§c!§7] §fPlugin §cSpawn §fwas disabled!");
     }
 
 }
