0c517732191983a091cb3edfac45cd9f4676f77c
KonusIntelliJ <62984001+KonusIntelliJ@users.noreply.github.com>
2022-07-10 19:40:46 +0300

    Update Hide.java

--- src/ru/konus/commands/Hide.java
+++ src/ru/konus/commands/Hide.java
@@ -9,6 +9,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 import ru.konus.main.Main;
 
 import java.util.ArrayList;
@@ -16,7 +17,7 @@ import java.util.List;
 
 public class Hide implements CommandExecutor, Listener {
 
-    public static List<Player> toggle = new ArrayList<>();
+    public static List<String> toggle = new ArrayList<>();
     private Main plugin;
 
     public Hide(Main plugin){
@@ -25,31 +26,28 @@ public class Hide implements CommandExecutor, Listener {
 
     @Override
     public boolean onCommand(CommandSender s, Command cmd, String label, String[] args) {
-        if(!cmd.getName().equalsIgnoreCase("hide")) return true;
-
-        if(!(s instanceof Player)){
+        if (!(s instanceof Player)){
             s.sendMessage("§cOnly Players!");
             return true;
         }
 
         Player p = (Player)s;
 
-        if(args.length != 0){
+        if (args.length != 0){
             p.sendTitle("§cПредупреждение", "§fИспользуйте §c/hide", 8, 20, 8);
             return true;
         }
 
-        if(toggle.contains(p)){
-            for (Player players : Bukkit.getOnlinePlayers()){
+        String namePlayer = p.getName();
+        if (toggle.contains(namePlayer)){
+            for (Player players : Bukkit.getOnlinePlayers())
                 p.showPlayer(plugin, players);
-            }
-            toggle.remove(p);
+            toggle.remove(namePlayer);
             p.sendTitle("§cСкрытие", "§fВы расскрыли всех игроков", 8, 18, 8);
         } else {
-            for (Player players : Bukkit.getOnlinePlayers()){
+            for (Player players : Bukkit.getOnlinePlayers())
                 p.hidePlayer(plugin, players);
-            }
-            toggle.add(p);
+            toggle.add(namePlayer);
             p.sendTitle("§aСкрытие", "§fВы скрыли всех игроков", 8, 18, 8);
         }
 
@@ -58,12 +56,17 @@ public class Hide implements CommandExecutor, Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e){
-        toggle.remove(e.getPlayer());
+        toggle.remove(e.getPlayer().getName());
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent e){
-        toggle.remove(e.getPlayer());
+        toggle.remove(e.getPlayer().getName());
+    }
+    
+    @EventHandler
+    public void onKick(PlayerKickEvent e){
+        toggle.remove(e.getPlayer().getName());
     }
 
 }
